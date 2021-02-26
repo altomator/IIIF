1f409ee74674da73c0f0154a0197075eb8c8a690
Jean-Philippe Moreux <jpmoreux@gmail.com>
2021-02-26 14:58:17 +0100

    Add files via upload

--- src-leaflet/app.js
+++ src-leaflet/app.js
@@ -1,1 +1,19 @@
+var map;
 
+map = L.map('map', {
+  center: [0, 0],
+  crs: L.CRS.Simple,
+  zoom: 0,
+});
+
+
+var layer1 = L.tileLayer.iiif('https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_natural_color_panorama_WIP2/info.json', {
+    fitBounds: false,
+  }
+).addTo(map);
+
+var layer2 = L.tileLayer.iiif(
+  'https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_combined_processes_WIP2_panorama_with_blue_water/info.json'
+).addTo(map);
+
+L.control.sideBySide(layer1, layer2).addTo(map);
