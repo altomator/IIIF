1b3de66bdc696dd32d0c66740d5534c6af81ec34
Jean-Philippe Moreux <jpmoreux@gmail.com>
2021-04-16 11:28:31 +0200

    Add files via upload

--- src-leaflet/app-btv1b10303825m.js
+++ src-leaflet/app-btv1b10303825m.js
@@ -7,13 +7,13 @@ map = L.map('map', {
 });
 
 
-var layer1 = L.tileLayer.iiif('http://free.iiifhosting.com/iiif/38b2bc7a898797778cd1d2ac9c86837c4cfd01ac6d05804aba03a390c56c8b6f/info.json', {
+var layer1 = L.tileLayer.iiif('https://free.iiifhosting.com/iiif/38b2bc7a898797778cd1d2ac9c86837c4cfd01ac6d05804aba03a390c56c8b6f/info.json', {
     fitBounds: true,
   }
 ).addTo(map);
 
 var layer2 = L.tileLayer.iiif(
-  'http://free.iiifhosting.com/iiif/639b11c8a996cdacdb39a3b22c06c4e07479881f05f65fa5993b2d5fd0942a87/info.json',
+  'https://free.iiifhosting.com/iiif/639b11c8a996cdacdb39a3b22c06c4e07479881f05f65fa5993b2d5fd0942a87/info.json',
   {fitBounds: true,}
 ).addTo(map);
 
