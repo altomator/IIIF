0e6e989cbe37b1ea4e034b8e9b6a3fd20efb883b
Jean-Philippe Moreux <jpmoreux@gmail.com>
2024-10-07 10:57:15 +0200

    Add files via upload

---
+++ observable/localize.js
@@ -0,0 +1,45 @@
+// The locale
+console.log("locale: ",myLocale);
+
+const translations = {
+  // English translations
+  "en": {
+    "title": "Digital collection history (full collection)",
+    "subtitle": "Source: Gallica and Gallica SRU API (2023/02)",
+    "li1": "OVERVIEW",
+    "li2": "PROVENANCE",
+    "li3": "PARTNERS",
+    "li4": "PARTNERS MAP",
+    "li10": "DATE and OCR",
+    "li11": "PROGRAMS",
+    "li12": "HISTORY"
+  },
+  "fr": {
+    "title": "Historique de la numérisation (collection complète)",
+    "subtitle": "Source : Gallica et API Gallica SRU (02/2023)",
+    "li1": "SYNTHÈSE",
+    "li3": "PARTENAIRES",
+    "li4": "CARTE PART.",
+    "li10": "DATE et OCR",
+    "li11": "PROGRAMMES",
+    "li12": "HISTORIQUE"
+
+  }
+};
+// When the page content is ready...
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    // Find all elements that have the key attribute
+    .querySelectorAll("[localize-key]")
+    .forEach(translateElement);
+});
+// Replace the inner text of the given HTML element
+// with the translation in the active locale,
+// corresponding to the element's data-i18n-key
+function translateElement(element) {
+  const key = element.getAttribute("localize-key");
+  const translation = translations[myLocale][key];
+  if (translation) {
+    element.innerText = translation;
+  }
+}
